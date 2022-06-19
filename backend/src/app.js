9cda9f347c5314dc270e56d09b99fe1747e8c1d0
Fernando Guerrero <devrrior@gmail.com>
2022-06-18 21:05:41 -0500

    agregando mysql2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const myql2 = require('mysql2');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// === http ===
 app.get('/:name/:id', (req, res) => {
   const { name, id } = req.params;
 
@@ -21,6 +23,40 @@ app.put('/', (req, res) => {
 });
 app.delete('/', (req, res) => {
   res.send('Peticion DELETE');
+});
+
+// === db ===
+const connection = myql2.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'tutorial',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('BD conectada');
+});
+
+// const querySQL = 'SHOW TABLES;';
+
+// connection.query(querySQL, (err, res) => {
+//   if (err) throw err;
+//   console.log('respuesta sql', res);
+// });
+
+const insertQuery = `INSERT INTO User(first_name, age) VALUES('Juan',30);`;
+
+connection.query(insertQuery, (err, res) => {
+  if (err) throw err;
+  console.log('respuesta insert', res);
+});
+
+const getQuery = `SELECT * FROM User;`;
+
+connection.query(getQuery, (err, res) => {
+  if (err) throw err;
+  console.log('respuesta get', res);
 });
 
 app.listen(3000, () => {
